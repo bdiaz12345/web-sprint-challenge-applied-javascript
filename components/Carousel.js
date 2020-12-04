508ebf62247cfdd7e4a1060c92fd52cee901eeeb
bdiaz12345 <bdiazmusic5@gmail.com>
2020-12-04 13:11:46 -0600

    stretch completed

--- components/Carousel.js
+++ components/Carousel.js
@@ -22,3 +22,60 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const dacarousel = () => {
+  const carousel = document.createElement('div');
+  const carouselImages = document.createElement('div');
+  const leftButton = document.createElement('div');
+  const mountainImage = document.createElement('img');
+  const computerImage = document.createElement('img');
+  const treesImage = document.createElement('img');
+  const turntable = document.createElement('img');
+  const rightButton = document.createElement('div');
+  mountainImage.src = 'https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg';
+  computerImage.src = 'https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg';
+  treesImage.src = 'https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg';
+  turntable.src = 'https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg';
+
+  carousel.appendChild(carouselImages);
+  carouselImages.appendChild(mountainImage);
+  carouselImages.appendChild(computerImage);
+  carouselImages.appendChild(treesImage);
+  carouselImages.appendChild(turntable);
+  carousel.appendChild(leftButton);
+  carousel.appendChild(rightButton);
+
+  carousel.classList.add('carousel');
+  carouselImages.classList.add('carousel-images');
+  leftButton.classList.add('left-button');
+  rightButton.classList.add('right-button');
+
+  leftButton.textContent = ' < ';
+  rightButton.textContent = ' > ';
+
+  const carouselFunction = () => {
+    var index = 0;
+    rightButton.addEventListener('click', (event) => {
+      if (index < 2){
+        console.log('index before', index)
+        index += 1;
+
+        document.querySelector('.carousel-images').style.left = (-50*index) + "%";
+      }
+    })
+    leftButton.addEventListener('click', (event) => {
+      console.log('left button clicked')
+      if (index > 0){
+        console.log('index before', index)
+        index -= 1;
+        console.log('index after', index)
+        carouselImages.style.left = (-50*index) + '%';
+      }
+    })
+  }
+  return (carouselFunction(), carousel);
+}
+
+const carouselDom = document.querySelector('.carousel-container');
+
+carouselDom.appendChild(dacarousel());
